a66830938dff4bfb6a6d1e5a50ee6ba164ef50df
chayliu <chayliu@126.com>
2021-06-19 13:24:37 +0800

    first commit

---
+++ 1_add_lib/main.cpp
@@ -0,0 +1,11 @@
+#include <math.h>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "1 + 2 = " << add(1, 2) << std::endl;
+    std::cout << "10 - 8 = " << sub(10, 8) << std::endl;
+
+    return 0;
+}
